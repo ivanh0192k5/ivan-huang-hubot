0c9655a321cefbb2856b2e46ecc6f49e1ed12da6
“Ivan <ivanh0192k5@gmail.com>
2017-05-20 19:03:33 -0700

    Bball cmd added

--- scripts/slackbot.js
+++ scripts/slackbot.js
@@ -27,12 +27,65 @@ module.exports = function(robot) {
   robot.hear(/hello/i, function(msg) {
     return msg.send("Hello there young lad");
   });
-  robot.hear(/sorry/i, function(msg) {
+  robot.hear(/bye/i, function(msg) {
     return msg.send("https://cdn.tinybuddha.com/wp-content/uploads/2015/07/Woman-Waving-Goodbye.png");
   });
   robot.hear(/lebron james/i, function(msg) {
     return msg.send("26.4 PPG, 8.6 RPG, 8.7 APG, 1.2 SPG, 54.8 FG% 36.3 3PT% https://cdn-s3.si.com/s3fs-public/styles/marquee_large_2x/public/2017/05/17/lebron-james-cavaliers-isaiah-thomas-breeze.jpg");
   });
+  robot.respond(/lebron james/i, function(msg) {
+    return msg.send("26.4 PPG, 8.6 RPG, 8.7 APG, 1.2 SPG, 54.8 FG% 36.3 3PT% https://cdn-s3.si.com/s3fs-public/styles/marquee_large_2x/public/2017/05/17/lebron-james-cavaliers-isaiah-thomas-breeze.jpg");
+  });
+  robot.hear(/stephen curry/i, function(msg) {
+    return msg.send("25.3 PPG, 4.5 RPG, 6.6 APG, 1.8 SPG, 46.8 FG% 41.1 3PT% https://cdn-s3.si.com/s3fs-public/styles/marquee_large_2x/public/2017/05/17/lebron-james-cavaliers-isaiah-thomas-breeze.jpg");
+  });
+  robot.respond(/stephen curry/i, function(msg) {
+    return msg.send("25.3 PPG, 4.5 RPG, 6.6 APG, 1.8 SPG, 46.8 FG% 41.1 3PT% https://cdn-s3.si.com/s3fs-public/styles/marquee_large_2x/public/2017/05/17/lebron-james-cavaliers-isaiah-thomas-breeze.jpg");
+  });
+  robot.hear(/kevin durant/i, function(msg) {
+    return msg.send("25.1 PPG, 8.3 RPG, 4.8 APG, 1.1 SPG, 53.7 FG% 37.3 3PT% https://cdn-s3.si.com/s3fs-public/styles/marquee_large_2x/public/2017/05/17/lebron-james-cavaliers-isaiah-thomas-breeze.jpg");
+  });
+  robot.respond(/kevin durant/i, function(msg) {
+    return msg.send("25.1 PPG, 8.3 RPG, 4.8 APG, 1.1 SPG, 53.7 FG% 37.3 3PT% https://cdn-s3.si.com/s3fs-public/styles/marquee_large_2x/public/2017/05/17/lebron-james-cavaliers-isaiah-thomas-breeze.jpg");
+  });
+  robot.hear(/kyrie irving/i, function(msg) {
+    return msg.send("25.2 PPG, 3.2 RPG, 5.8 APG, 1.2 SPG, 47.3 FG% 40.1 3PT% https://cdn-s3.si.com/s3fs-public/styles/marquee_large_2x/public/2017/05/17/lebron-james-cavaliers-isaiah-thomas-breeze.jpg");
+  });
+  robot.respond(/kyrie irving/i, function(msg) {
+    return msg.send("25.2 PPG, 3.2 RPG, 5.8 APG, 1.2 SPG, 47.3 FG% 40.1 3PT% https://cdn-s3.si.com/s3fs-public/styles/marquee_large_2x/public/2017/05/17/lebron-james-cavaliers-isaiah-thomas-breeze.jpg");
+  });
+  robot.hear(/nba finals/i, function(msg) {
+    return msg.send("The NBA finals starts on June 1st. With the likely matchup of Warriors vs Cavs.");
+  });
+  robot.respond(/Is (.*) a good basketball player/i, function(msg) {
+  var name;
+  name = msg.match[1];
+  if (name == "lebron james"){
+    return msg.send("Yes");
+  } else {
+    return msg.reply("No");
+  }
+  robot.hear(/who is your favorite (.*)/, function(msg) {
+  var fav;
+  fav = msg.match[1];
+  console.log(fav);
+  switch (fav) {
+    case "basketball player":
+      return msg.reply("My favorite player is kobe bryant");
+      break;
+    case "football player":
+      return msg.reply("None, I only know about basketball");
+      break;
+    case "baseball player":
+      return msg.reply("Michael Jordan");
+      break;
+    default:
+      return msg.reply("I don't know what that is. Is it related to basketball? Because, that is all I know.");
+  }
+});
+
+});
+  
   
 
 
